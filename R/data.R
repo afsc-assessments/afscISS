5294aab9fa6281c19c416a99a349d5032b0d432a
pete-hulson <pete.hulson@noaa.gov>
2024-06-11 14:47:12 -0800

    build package with data try

--- R/data.R
+++ R/data.R
@@ -19,31 +19,42 @@ pkg_dta <- function(append = TRUE) {
     ai <- purrr::map(list.files(here::here('output', 'ai')), ~ vroom::vroom(here::here('output', 'ai', .)))
     names(ai) <- list.files(here::here('output', 'ai')) %>% 
       stringr::str_replace(., '.csv', "")
-    usethis::use_data(ai)
     
     # ebs results ----
     ebs <- purrr::map(list.files(here::here('output', 'ebs')), ~ vroom::vroom(here::here('output', 'ebs', .)))
     names(ebs) <- list.files(here::here('output', 'ebs')) %>% 
       stringr::str_replace(., '.csv', "")
-    usethis::use_data(ebs)
     
     # ebs_slope results ----
     ebs_slope <- purrr::map(list.files(here::here('output', 'ebs_slope')), ~ vroom::vroom(here::here('output', 'ebs_slope', .)))
     names(ebs_slope) <- list.files(here::here('output', 'ebs_slope')) %>% 
       stringr::str_replace(., '.csv', "")
-    usethis::use_data(ebs_slope)
     
     # goa results ----
     goa <- purrr::map(list.files(here::here('output', 'goa')), ~ vroom::vroom(here::here('output', 'goa', .)))
     names(goa) <- list.files(here::here('output', 'goa')) %>% 
       stringr::str_replace(., '.csv', "")
-    usethis::use_data(goa)
     
     # nebs results ----
     nebs <- purrr::map(list.files(here::here('output', 'nebs')), ~ vroom::vroom(here::here('output', 'nebs', .)))
     names(nebs) <- list.files(here::here('output', 'nebs')) %>% 
       stringr::str_replace(., '.csv', "")
-    usethis::use_data(nebs)
+    
+    # write out results as package data ----
+    
+    data_iss <- list(ai, ebs, ebs_slope, goa, nebs)
+    names(data_iss) <- c('ai', 'ebs', 'ebs_slope', 'goa', 'nebs')
+    usethis::use_data(data_iss)
   }
 
 }
+
+#' Results from surveyISS package
+#'
+#' A list containing the various output dataframes from the surveyISS package
+#'
+#' @format A list
+#' \describe{
+#' A list of ISS, age/length pop'n numbers, bootstrap bias, mean length-at-age, replicated rss, replicated age/length pop'n numbers
+#' }
+"data_iss"
