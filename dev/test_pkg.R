396afa342c8e34cd3c9bd3acf932f75cec27e651
pete-hulson <pete.hulson@noaa.gov>
2024-06-11 16:31:21 -0800

    messing with data.table dependency

--- dev/test_pkg.R
+++ dev/test_pkg.R
@@ -1,7 +1,7 @@
 # script to obtain age/length input sample size from production run
 
 # load packages ----
-devtools::unload('afscISS')
+# devtools::unload('afscISS')
 devtools::install_github("afsc-assessments/afscISS", force = TRUE)
 library(afscISS)
 
@@ -9,29 +9,29 @@ library(afscISS)
 
 afscISS::get_ISS(species = 21720,
                  region = 'goa',
-                 type = 'age',
+                 comp = 'age',
                  sex_cat = 4)
 
 afscISS::get_ISS(species = 21720,
                  region = 'goa',
-                 type = 'length',
+                 comp = 'length',
                  sex_cat = 4)
 
 afscISS::get_ISS(species = 21720,
                  region = 'goa',
-                 type = 'age',
+                 comp = 'age',
                  sex_cat = 4,
                  spec_case = 'w_c_egoa')
 
 
 afscISS::get_ISS(species = 21720,
                  region = 'ai',
-                 type = 'age',
+                 comp = 'age',
                  sex_cat = 4,
                  spec_case = 'ai_subreg')
 
 
 afscISS::get_ISS(species = 21720,
                  region = 'goa',
-                 type = 'caal',
-                 sex_cat = 4)
+                 comp = 'caal',
+                 sex_cat = 0)
