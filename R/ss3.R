76b357461e66983aa0e1a40ff9a189e248177dd3
Ben Williams <ben.williams@noaa.gov>
2024-07-25 15:20:41 -0800

    correction

--- R/ss3.R
+++ R/ss3.R
@@ -1,7 +1,13 @@
-get_ss3_comp <- function(species, region, comp, sex_cat=12, bysex = FALSE, spec_case=NULL,
+get_ss3_comp <- function(species, region, comp='age', sex_cat=12, bysex = FALSE, spec_case=NULL,
                          max_age=12, seas=1, flt=2, part=0, Ageerr=1, Lbin_lo=1, Lbin_hi=-1) {
-  if(comp=='caal' & sex_cat!=0) {stop('must set: sex_cat=0 for CAAL')}
   if(isTRUE(bysex)){cat('Ignoring the sex_cat and sex will be set as 1 & 2\n\n')}
+  
+  
+  iss = get_ISS(species = species,
+                region = region,
+                comp = comp,
+                sex_cat = sex_cat,
+                spec_case = spec_case)
   
   if(isFALSE(bysex)){
     dat = get_comp(species = species,
@@ -9,48 +15,18 @@ get_ss3_comp <- function(species, region, comp, sex_cat=12, bysex = FALSE, spec_
                    comp = comp,
                    sex_cat = sex_cat,
                    spec_case = spec_case)
-    
-    # full dataset
-    expand.grid(year = years, species_code = unique(data$species_code), sex = sexes,
-                age = bin) %>% 
-      merge(dat, all=T) %>% 
-      tidytable::left_join(tidytable::select(iss, year, iss)) %>% 
-      tidytable::replace_na(list(prop = 0)) %>%
-      tidytable::mutate(prop = tidytable::case_when(age>=max_age ~ sum(prop),
-                                                    TRUE ~ prop),
-                        .by = c(sex, year)) %>% 
-      tidytable::filter(age<=max_age) -> df
-    
   } else {
-    dat = get_comp(species = species,
-                   region = region,
-                   comp = comp,
-                   sex_cat = 1,
-                   spec_case = spec_case)
-    
-    dat2 = get_comp(species = species,
-                    region = region,
-                    comp = comp,
-                    sex_cat = 2,
-                    spec_case = spec_case)
-    
-    # full dataset
-    expand.grid(year = years, species_code = unique(data$species_code), sex = sexes,
-                age = bin) %>% 
-      merge(tidytable::bind_rows(dat, dat2), all=T) %>% 
-      tidytable::left_join(tidytable::select(iss, year, iss)) %>% 
-      tidytable::replace_na(list(prop = 0)) %>%
-      tidytable::mutate(prop = tidytable::case_when(age>=max_age ~ sum(prop),
-                                                    TRUE ~ prop),
-                        .by = c(sex, year)) %>% 
-      tidytable::filter(age<=max_age) -> df
-    
+    dat = tidytable::bind_rows(get_comp(species = species,
+                                        region = region,
+                                        comp = comp,
+                                        sex_cat = 1,
+                                        spec_case = spec_case),
+                               get_comp(species = species,
+                                        region = region,
+                                        comp = comp,
+                                        sex_cat = 2,
+                                        spec_case = spec_case))
   }
-  iss = get_ISS(species = species,
-                region = region,
-                comp = comp,
-                sex_cat = sex_cat,
-                spec_case = spec_case)
   
   # setup ----
   years = unique(dat$year)
@@ -61,6 +37,16 @@ get_ss3_comp <- function(species, region, comp, sex_cat=12, bysex = FALSE, spec_
   nsex = length(sexes)
   
   
+  # full dataset
+  expand.grid(year = years, species_code = unique(dat$species_code), sex = sexes,
+              age = bin) %>% 
+    merge(dat, all=T) %>% 
+    tidytable::left_join(tidytable::select(iss, year, iss)) %>% 
+    tidytable::replace_na(list(prop = 0)) %>%
+    tidytable::mutate(prop = tidytable::case_when(age>=max_age ~ sum(prop),
+                                                  TRUE ~ prop),
+                      .by = c(sex, year)) %>% 
+    tidytable::filter(age<=max_age) -> df
   
   if(isFALSE(bysex)) {
     df %>% 
@@ -88,7 +74,7 @@ get_ss3_comp <- function(species, region, comp, sex_cat=12, bysex = FALSE, spec_
                         Lbin_hi = Lbin_hi) %>% 
       tidytable::filter(sex==2) %>% 
       tidytable::pivot_wider(names_from = age, values_from = prop, names_prefix = 'f') %>% 
-      tidytable::left_join(df2 %>% 
+      tidytable::left_join(df %>% 
                              tidytable::filter(sex==1) %>% 
                              tidytable::pivot_wider(names_from = age, values_from = prop, names_prefix = 'm') %>% 
                              tidytable::select(-sex)) %>% 
